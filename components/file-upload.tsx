5da402b4d678cb49e3a1ee13eee04c855d3b0958
Rahul Parikh <rahulparikh927@outlook.com>
2023-11-13 11:22:29 +0100

    Server and chat channel: setup and manage

---
+++ components/file-upload.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { X } from "lucide-react";
+import Image from "next/image";
+
+import { OurFileRouter } from "@/app/api/uploadthing/core";
+import { UploadDropzone } from "@/lib/uploadthing";
+
+interface FileUploadProps {
+  onChange: (url?: string) => void;
+  value: string;
+  endpoint: keyof OurFileRouter;
+}
+
+export const FileUpload = ({ value, endpoint, onChange }: FileUploadProps) => {
+  const fileType = value?.split(".").pop();
+
+  if (value && fileType !== "pdf") {
+    return (
+      <div className="relative h-20 w-20">
+        <Image fill src={value} alt="server image" className="rounded-full" />
+        <button
+          className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0  shadow-sm"
+          type="button"
+          onClick={() => onChange("")}
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <UploadDropzone
+      endpoint={endpoint}
+      onClientUploadComplete={(res) => {
+        onChange(res?.[0].url);
+      }}
+      onUploadError={(error: Error) => {
+        console.log(error);
+      }}
+    />
+  );
+};
